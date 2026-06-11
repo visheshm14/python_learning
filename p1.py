857b81cb0bf63df920d18698ddcc8bc96bada7d1
Vishesh Mishra <visheshmishra149@gmail.com>
2026-06-11 20:10:38 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,3 @@
+print("hello world")
+
+print("Python is one of the most popular programming languages. It’s simple to use, packed with features and supported by a wide range of libraries and frameworks. Its clean syntax makes it beginner-friendly.")
