929810cb95318b1c40c8713083065d27f32198ee
Vishesh Mishra <visheshmishra149@gmail.com>
2026-06-23 13:53:28 +0530

    Add files via upload

---
+++ p10.py
@@ -0,0 +1,18 @@
+name = "vishesh"
+
+nameshort = name[0:3]
+print(nameshort)
+
+nameshort = name[-4:-1]
+print(nameshort)
+
+nameshort = name[1:6:2]
+print(nameshort)
+
+char1 = name[2]
+print(char1)
+
+print(len(name))
+
+print(name.endswith("sh"))
+print(name.startswith("vi"))
