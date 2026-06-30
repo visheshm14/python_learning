c351f652c5e0cb3ceac22ee1452ef3cbb5af5b8e
Vishesh Mishra <visheshmishra149@gmail.com>
2026-06-30 09:52:40 +0530

    Add files via upload

---
+++ p12.py
@@ -0,0 +1,20 @@
+l=[1,4,2,7,9,3,8,10,6,5]
+
+print(l)
+l.sort()
+print(l)
+l.reverse()
+print(l)
+l.append(11)
+print(l)
+l.insert(3,8)
+print(l)
+l.pop(3)
+print(l)
+l.remove(2)
+print(l)
+
+d= input("enter")
+l.append(d)
+print(l)
+
