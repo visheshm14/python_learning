976bd547518feee848e1b0188ec26cd82d969f0c
Vishesh Mishra <visheshmishra149@gmail.com>
2026-07-02 09:38:38 +0530

    Add files via upload

---
+++ p14.py
@@ -0,0 +1,17 @@
+dict={"vishesh":12
+      ,"afssd":13
+      ,"werggd":15
+      }
+
+print(dict, type(dict))
+
+dict.items()
+print(dict)
+
+print(dict.keys())
+print(dict.values())
+
+dict.update({"vishesh":44})
+print(dict)
+
+print(dict.get("vishesh"))
