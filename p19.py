273a6d98aa1df99585166ba0cf8a809a0fc57300
Vishesh Mishra <visheshmishra149@gmail.com>
2026-07-07 12:02:41 +0530

    Add files via upload

---
+++ p19.py
@@ -0,0 +1,14 @@
+def avg():
+    a = int(input("enter yout no 1"))
+    b = int(input("enter yout no 2"))
+    c = int(input("enter yout no 3"))
+
+    average=(a+b+c)/3
+
+    print(average)
+
+avg()
+avg()
+avg()
+avg()
+avg() 
