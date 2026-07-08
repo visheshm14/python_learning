47fe053d396ced62a12237377c72432c0affb799
Vishesh Mishra <visheshmishra149@gmail.com>
2026-07-08 19:42:11 +0530

    Add files via upload

---
+++ p20.py
@@ -0,0 +1,13 @@
+def greatest(a,b,c):
+    if(a>b and a>c):
+        return a
+    elif(b>a and  b>c):
+        return b
+    elif(c>b and c>a):
+        return c
+    
+a=1 
+b=22
+c=4
+
+print(greatest(a,b,c))
